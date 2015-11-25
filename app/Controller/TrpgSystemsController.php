7ff832efbef5ea8637868d7ac689c2c80c9fdba8
agekke <agekke@AGHR1018>
2015-11-25 11:47:46 +0900

    TRPGシステム一覧の表示。

--- app/Controller/TrpgSystemsController.php
+++ app/Controller/TrpgSystemsController.php
@@ -1,4 +1,29 @@
 <?php
+/**
+ * TRPGシステムのコントローラー。
+ *
+ * @author Hidemasa Aoki
+ */
 class TrpgSystemsController extends AppController {
-	public $scaffold;
+
+	/**
+	 * 初期表示。
+	 */
+	public function index() {
+
+		// TRPGシステム一覧の取得
+		$this->set('trpgSystems', $this->getTrpgSystems());
+	}
+
+	/**
+	 * TRPGシステム一覧の取得。
+	 */
+	protected function getTrpgSystems() {
+		$options = array(
+//				'order' => array('TrpgSystem.rank ASC', 'TrpgSystem.modified DESC'),
+				'order' => array('TrpgSystem.introduction_order ASC', 'TrpgSystem.modified DESC'),
+				'limit' => 5,
+		);
+		return $this->TrpgSystem->find('all', $options);
+	}
 }
