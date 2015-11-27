e677c5aff50e06d547f69f1d1b07f50034ec33fd
agekke <agekke@AGHR1018>
2015-11-27 11:31:39 +0900

    ニュース一覧の作成。

--- app/Controller/NewsController.php
+++ app/Controller/NewsController.php
@@ -1,4 +1,30 @@
 <?php
+/**
+ * TRPGシステムのコントローラー。
+ *
+ * @author Hidemasa Aoki
+ */
 class NewsController extends AppController {
-	public $scaffold;
+
+	/**
+	 * 初期表示。
+	 */
+	public function index() {
+
+		// ニュース一覧の取得
+		$this->getNews();
+	}
+
+	/**
+	 * ニュース一覧の取得。
+	 */
+	protected function getNews() {
+		$this->paginate = array(
+				'News' => array(
+					'order' => array('News.delivery_time' => 'DESC', 'News.modified' => 'DESC'),
+					'limit' => 10,
+				)
+		);
+		$this->set('news', $this->paginate('News'));
+	}
 }
